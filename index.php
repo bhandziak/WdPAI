26465a4b4e2514db402285f85567344899ea2e78
bhandziak <64088278+bhandziak@users.noreply.github.com>
2025-10-07 19:44:27 +0200

    added connection to postgres

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 
-echo "<h1>Witaj student</h1>"
+echo "<h1>Witaj student</h1>";
 
+$host = 'db';
+$port = '5432';
+$dbname = 'db';
+$user = 'docker';
+$password = 'docker';
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+
+    $stmt = $pdo->query("SELECT * FROM students");
+
+    echo "<ul>";
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "<li>{$row['id']}. {$row['name']} {$row['surname']}</li>";
+    }
+    echo "</ul>";
+
+} catch (PDOException $e) {
+}
+
+?>
